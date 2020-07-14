4246a8632a72ce1d48417d644224235ee4aca267
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-07-13 22:16:18 -0300

    autenticacao

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 
-const App: React.FC = () => (
-  <>
-    {/* <SignIn /> */}
-    <SignIn />
+import { AuthProvider } from './context/AuthContext';
+
+const App: React.FC = () => {
+  return (
+    <>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
     <GlobalStyle />
   </>
-);
+  )
+}
 
 export default App;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-unresolved */
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import { useAuth } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.svg';
@@ -14,15 +15,22 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
+  const { signIn } = useAuth();
 
-  const handleSubmit = useCallback(async (data: object) => {
+  console.log(user);
+
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({})
 
-      console.log(formRef)
       const schema = Yup.object().shape({
         email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
         password: Yup.string().required('Senha obrigatória'),
@@ -31,42 +39,49 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
-    } catch (err) {
 
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
+    } catch (err) {
       const errors = getValidationErrors(err);
+
       formRef.current?.setErrors(errors)
 
     }
-  }, []);
-return (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <Form ref={formRef} onSubmit={handleSubmit} >
-        <h1>Faça seu logon</h1>
+  }, [signIn]);
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form ref={formRef} onSubmit={handleSubmit} >
+          <h1>Faça seu logon</h1>
 
-        <Input
-          name="password"
-          icon={FiLock}
-          placeholder="Senha"
-          type="password"
-        />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Button type="submit">Entrar</Button>
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="Senha"
+            type="password"
+          />
 
-        <a href="forgot"> Esqueci minha senha</a>
-      </Form>
+          <Button type="submit">Entrar</Button>
 
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+          <a href="forgot"> Esqueci minha senha</a>
+        </Form>
 
-    <Background />
-  </Container>
-)};
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
